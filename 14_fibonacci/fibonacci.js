24e9151449a1e3ea764b7a044c23b3ba661deaf4
Itay Levi <itaylh33@gmail.com>
2026-01-11 20:37:03 +0200

    exercise 14 (fuck this shit)

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let num;
+    if (typeof n !== "number") {
+        num = Number(n)
+    } else {
+        num = n
+    }
+    if (num < 0) return "OOPS";
+    if (num === 0) return 0;
+    if (num === 1) return 1;
 
+    let prePreviousItem = 0;
+    let previousItem = 1;
+    let current;
+
+    for (let i = 2; i <= num; i++) {
+        current = prePreviousItem + previousItem;
+        [prePreviousItem, previousItem] = [previousItem, current]
+    }
+    return current
 };
 
 // Do not edit below this line
