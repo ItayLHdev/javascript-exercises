88f54ccdbecab14a41655cc64356adf841d33326
Itay Levi <itaylh33@gmail.com>
2026-01-11 22:21:55 +0200

    exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    const newArr = arr.map((item) => {
+        if (!("yearOfDeath" in item)) {
+            item.yearOfDeath = currentYear;
+        }
+        return item;
+    })
+    
+    newArr.sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))
+    return newArr[0]
 };
 
 // Do not edit below this line
