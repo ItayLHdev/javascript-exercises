03651e61268809c0bb8a488c4c90743f8b564df9
Itay Levi <itaylh33@gmail.com>
2026-01-11 20:04:08 +0200

    exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const characters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const newString = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => characters.includes(char))
+        .join('')
+    const reversedPalindrome = newString.split('').reverse().join('')
+    return newString === reversedPalindrome;
 };
 
 // Do not edit below this line
